5d257abdf7582fdd3e3a27c92aaeaa8554aaa648
MANISH PATEL <rmanish2000@gmail.com>
2026-09-21 10:39:11 +0530

    Remove generated build output from A-018

--- app/dist/server/_ssr/empty-plugin-adapters-Dq-X_n2v.mjs
+++
@@ -1,5 +0,0 @@
-//#region ../../../dev-server/node_modules/.nitro/vite/services/ssr/assets/empty-plugin-adapters-Dq-X_n2v.js
-var pluginSerializationAdapters = [];
-var hasPluginAdapters = false;
-//#endregion
-export { hasPluginAdapters, pluginSerializationAdapters };
